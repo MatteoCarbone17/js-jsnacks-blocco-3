81d0bdcb1c75da45f77e37405389592456e1c02c
MatteoCarbone17 <matteocarbone17@live.com>
2022-11-28 16:24:51 +0100

    numeri tra 1 e 10

---
+++ snack-1/script.js
@@ -0,0 +1,18 @@
+
+// // Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto//
+
+
+
+
+
+
+
+let userNumber = parseInt(prompt('scegli un numero da 1 a 10'))
+
+while ( userNumber < 10 ) {
+    userNumber = parseInt(prompt('scegli un numero da 1 a 10'))
+    i++;
+    
+}
+
+console.log(userNumber )
